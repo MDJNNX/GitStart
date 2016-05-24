f46ebdbc4aaaa2f34c736e5cd467b177e87067b7
MDJNNX <806918093@qq.com>
2016-05-24 13:32:59 +0800

    Add Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test
+{
+	public static void main(String[] args)
+	{
+		Console cons = System.console();
+		String username = cons.readLine("user name:");
+		char[] passwd = cons.readPassword("Password: ");
+		System.out.println(username);
+		System.out.println(passwd);
+	}
+}
